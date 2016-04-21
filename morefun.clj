8fc3484f2e2ab8a8f986d95e86f8b18bb419194c
sailajaappari <sailajakadali@gmail.com>
2016-04-21 15:55:54 +0530

    Higher order functions clojure koans video8

--- morefun.clj
+++ morefun.clj
@@ -7,5 +7,8 @@
           (< x 30)) [10 20 30 40 50])
 (map (fn [x]
           (< x 30)) [10 20 30 40 50])
-(filter (fn [x y]
-             (* x y)) [1 2 3 4 5])
+
+(filter (fn [[k v]]
+            (even? k)) {1 "a", 2 "b", 3 "c", 4 "d"})
+(filter (fn [x]
+            (= (count x) 1)) ["a" "ab" "b" "bc" "c" "abc" "cdef"])
