48cc6a9e66f17f241fe52f7b2b6578c40139870e
sailajaappari <sailajakadali@gmail.com>
2016-05-02 15:50:11 +0530

    Modified version of perfect function

--- application/src/application/perfect.clj
+++ application/src/application/perfect.clj
@@ -1,15 +1,11 @@
-(def sum 0)
-(def cnt 1)
+
 (defn is-perfect
-  ([num] (is-perfect num cnt sum))
+  ([num] (is-perfect num 1 0))
   ([num cnt sum]
    (if (> cnt (/ num 2))
-       (if (= num sum)
-           true
-           false)
+       (= sum num)
        (if (= (rem num cnt) 0)
-           (is-perfect num (inc cnt) (+ sum cnt))
-           (is-perfect num (inc cnt) sum))
-      )))
+          (is-perfect num (inc cnt) (+ sum cnt))
+          (is-perfect num (inc cnt) sum)))))
 (is-perfect 6)
 (is-perfect 100)
